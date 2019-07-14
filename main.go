27dec4bc67455f77f0958726e0652e88ba5df7f1
Maxim Klippa <maxim.klippa@gmail.com>
2019-07-15 00:00:50 +0300

    akward solution

--- main.go
+++ main.go
@@ -7,35 +7,83 @@ import (
 
 type job func(in, out chan interface{})
 
-func startWorker(in <-chan job) {
-	inCh := make(chan interface{}, 0)
-	outCh := make(chan interface{}, 0)
-	for input := range in {
-		input(inCh, outCh)
-		inCh <- outCh
-		runtime.Gosched() // попробуйте закомментировать
-	}
-}
-
 func main() {
 	runtime.GOMAXPROCS(0)
-	workers := make(chan job, 2)
-	go startWorker(workers)
 
 	jobs := []job{
-		job(func(in, out chan interface{}) { out <- 1 }),
-		job(func(in, out chan interface{}) { out <- in }),
-		job(func(in, out chan interface{}) { out <- in }),
-		job(func(in, out chan interface{}) { out <- in }),
-		job(func(in, out chan interface{}) { out <- in }),
-		job(func(in, out chan interface{}) { out <- in }),
-		job(func(in, out chan interface{}) { <-in }),
+		job(func(in, out chan interface{}) {
+			out <- 1
+			out <- 2
+			out <- 3
+		}),
+		job(func(in, out chan interface{}) {
+			for val := range in {
+				out <- val
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for val := range in {
+				out <- val
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for val := range in {
+				out <- val
+			}
+		}),
+		job(func(in, out chan interface{}) {
+			for val := range in {
+				fmt.Println(val)
+			}
+		}),
 	}
 
-	for _, worker := range jobs {
-		workers <- worker
+	workerInput := make(chan interface{}, 2)
+	combination := make(chan interface{})
+	for i := 0; i < 100; i++ {
+		go func(in <-chan interface{}) {
+			for input := range in {
+				inCh := make(chan interface{})
+				outCh := make(chan interface{}, 1)
+				outCh <- input
+				close(outCh)
+				for i := 1; i < len(jobs)-1; i++ {
+					inCh, outCh = outCh, make(chan interface{})
+					go func(i int, in, out chan interface{}) {
+						jobs[i](in, out)
+						fmt.Println("foo")
+						close(out)
+					}(i, inCh, outCh)
+				}
+				combination <- (<-outCh)
+			}
+		}(workerInput)
 	}
-	close(workers)
 
-	fmt.Scanln()
+	works := make(chan interface{}, 100)
+	jobs[0](nil, works)
+	close(works)
+
+	for data := range works {
+		workerInput <- data
+	}
+	close(workerInput)
+
+	go func() {
+		jobs[len(jobs)-1](combination, nil)
+		close(combination)
+	}()
+
+	// in := make(chan interface{})
+	// out := make(chan interface{})
+
+	// for i := 0; i < len(jobs); i++ {
+	// 	in, out = out, make(chan interface{})
+	// 	go func(i int, in, out chan interface{}) {
+	// 		jobs[i](in, out)
+	// 		close(out)
+	// 	}(i, in, out)
+	// }
+
+	// fmt.Scanln()
 }
